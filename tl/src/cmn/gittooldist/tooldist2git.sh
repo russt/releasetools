bd73bbb5c8a6eab283c19b596164d14217c48443
Russ Tremain <russt@releasetools.org>
2013-08-23 17:07:08 -0700

    add tooldist2git, which provides git tools distributions

--- tl/src/cmn/gittooldist/tooldist2git.sh
+++ tl/src/cmn/gittooldist/tooldist2git.sh
@@ -1,12 +1,136 @@
-#tooldist2git.sh - sync cvs tools dist to git.
-#definitions:
-# INPUTS:
+#!/bin/sh
+#
+# BEGIN_HEADER - DO NOT EDIT
+# @(#)tooldist2git.sh
+# 
+# Author:  Russ Tremain
+# 
+# Copyright (c) 2013-2013 Russ Tremain. All Rights Reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PERFORCE 
+# SOFTWARE, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+# DAMAGE.
+# 
+# END_HEADER - DO NOT EDIT
+#
+
+
+#tooldist2git - sync cvs tools dist to git.
+#
+# INPUT:
 #    CVS_TOOLDIST_PATH - base of cvs tools distribution repository, eg. /scm/cvs/tooldist/rtproj/main/rttl
 #    GIT_TOOLDIST_PATH - base of git tools distribution repository, e.g. /scm/git/public/tooldist/rtproj/main/rttl
 # 
-# TODO: allow syncs to be incremental. NOTE:  cannot destroy master git repo and recreate, will screw up clients.
-# TODO: hook up to toolsBuild script (if GIT_TOOLDIST_PATH is defined)
+# OUTPUT:
+#    git repository in GIT_TOOLDIST_PATH, one branch per cvs tools module definition
 #
+
+##################################### USAGE ####################################
+
+usage()
+{
+    status=$1
+
+    cat << EOF
+Usage:  $p [options] [var=def...] [platforms]
+
+ Sync cvs tools distribution to corresponding git repository.
+
+ Each cvs tools module (linuxtools, osxtools, etc) is synced to a git branch
+ of the same name.
+
+Options:
+ -help        Display this help screen.
+ -verbose     show informational messages.
+ -clean       initalize a new git repository and working directories.
+              NOTE: -clean should be used sparingly, as it requires git
+              clients of distribution to reclone their tools.
+
+Environment:
+ CVS_TOOLDIST_PATH - base of cvs tools distribution repository, eg. /scm/cvs/tooldist/rtproj/main/rttl
+ GIT_TOOLDIST_PATH - base of git tools distribution repository, e.g. /scm/gitroot/public/tooldist/rtproj/main/rttl
+ GIT_TOOLDIST_WORK - base of git working directories, used to sync the cvs tools modules to git branches
+
+Example:
+ Sync the current CVS tools distribution to git:
+   $p -clean GIT_TOOLDIST_PATH=/scm/gitroot/public/tooldist/rtproj/main/rttl GIT_TOOLDIST_WORK=/scm/gitroot/tmp/gitsync
+
+ Checkout the git tools distribution for linux:
+   git clone --branch linuxtools git://localhost/tooldist/rtproj/main/rttl linuxtools
+
+ Checkout the git tools distribution for macosx:
+   git clone --branch macosxtools git://localhost/tooldist/rtproj/main/rttl macosxtools
+
+EOF
+
+    exit $status
+}
+
+parse_args()
+{
+    init_defaults
+
+    while [ $# -gt 0 -a "$1" != "" ]
+    do
+        arg=$1; shift
+
+        case $arg in
+        -help )
+            usage 0
+            ;;
+        -clean )
+            CLEAN_DIST=1
+            ;;
+        *=* )
+            tmp=`echo $arg|sed -e 's/"/\\\\"/g'`
+            #echo A arg=.$arg. tmp is .$tmp.
+            tmp=`echo $tmp|sed -e 's/^\([^=][^=]*\)=\(.*\)/\1="\2"; export \1/'`
+            #echo B tmp is .$tmp.
+            eval $tmp
+            ;;
+        -* )
+            echo "${p}: unknown option, $arg"
+            usage 1
+            ;;
+        * )
+            if [ -z "$PLATFORM_ARGS" ]; then
+                PLATFORM_ARGS="$arg"
+            else
+                PLATFORM_ARGS="$PLATFORM_ARGS $arg"
+            fi
+            ;;
+        esac
+    done
+${SH_PARSE_ARGS_EPILOG:undef}
+}
+
+init_defaults()
+#this routine is responsible for initializing all default parameters
+{
+    PLATFORM_ARGS=
+    CLEAN_DIST=0
+}
+
+################################## SUBROUTINES #################################
 
 createGitToolDistRepo()
 {
@@ -16,10 +140,10 @@ createGitToolDistRepo()
 }
 
 createGitWorking()
-#INPUTS:  TMPWORKROOT, GIT_TOOLDIST_PATH, module
-#OUTPUT:  new clone from $GIT_TOOLDIST_PATH in $TMPWORKROOT/$module, with proper ignores.
+#INPUTS:  GIT_TOOLDIST_WORK, GIT_TOOLDIST_PATH, module
+#OUTPUT:  new clone from $GIT_TOOLDIST_PATH in $GIT_TOOLDIST_WORK/$module, with proper ignores.
 {
-    cd $TMPWORKROOT
+    cd $GIT_TOOLDIST_WORK
 
     #create subdir as part of clone
     git clone $GIT_TOOLDIST_PATH $module
@@ -35,23 +159,34 @@ EOF
     #can persist between runs, they may get browsed.
 }
 
+check_set_env()
+#check that needed environment is provided. return 0 if okay.
+{
+    _ckenv_err=0
+
+    [ -z "$CVS_TOOLDIST_PATH" ] && _ckenv_err=1 && bldmsg -error -p $p CVS_TOOLDIST_PATH must be defined.
+    [ -z "$GIT_TOOLDIST_PATH" ] && _ckenv_err=2 && bldmsg -error -p $p GIT_TOOLDIST_PATH must be defined.
+    [ -z "$GIT_TOOLDIST_WORK" ] && _ckenv_err=2 && bldmsg -error -p $p GIT_TOOLDIST_WORK must be defined.
+
+    return $_ckenv_err
+}
+
 ##################################### MAIN #####################################
 
 p=`basename $0`
+exit_status=0
 
-CLEAN_DIST=1
-CLEAN_DIST=0
+parse_args "$@"
 
-#this project:
-GITROOT=/scm/gitroot
+check_set_env
+if [ $? -ne 0 ]; then
+    bldmsg -error -p $p One or more needed environment variables are not defined, abort.
+    exit 1
+fi
 
-CVS_TOOLDIST_PATH=/scm/cvs/tooldist/rtproj/main/rttl
-GIT_TOOLDIST_PATH=$GITROOT/public/tooldist/rtproj/main/rttl
-
-TMPWORKROOT=$GITROOT/tmp/gitsync
 if [ $CLEAN_DIST -eq 1 ]; then
     bldmsg -p $p -warn Cleaning git tools repository and working directories - clients should re-clone
-    rm -rf $TMPWORKROOT
+    rm -rf $GIT_TOOLDIST_WORK
     rm -rf $GIT_TOOLDIST_PATH
 fi
 
@@ -59,44 +194,43 @@ fi
 [ ! -d $GIT_TOOLDIST_PATH ] && createGitToolDistRepo
 
 #create work-dir root if not present:
-mkdir -p "$TMPWORKROOT"
+mkdir -p "$GIT_TOOLDIST_WORK"
 
-cvsmodules=`cvs -d $CVS_TOOLDIST_PATH co -c | grep tools | sed -e 's/[ ].*//'`
+if [ -z "$PLATFORM_ARGS" ]; then
+    cvsmodules=`cvs -d $CVS_TOOLDIST_PATH co -c | grep tools | sed -e 's/[ ].*//'`
+else
+    cvsmodules="$PLATFORM_ARGS"
+fi
 
-cvsmodules=linuxtools
-echo TESTING cvsmodules=$cvsmodules
-
-# git co -b linuxtools
-# cvs -f -d /scm/cvs/tooldist/rtproj/main/rttl co linuxtools
-# git add --all
-# git commit -m "add to branch linuxtools"
-# git push origin linuxtools
-
+#########
+# foreach cvs module...
+#########
 for module in $cvsmodules
 do
-    gitworkdir=$TMPWORKROOT/$module
+    gitworkdir=$GIT_TOOLDIST_WORK/$module
 
-    cd $TMPWORKROOT
+    cd $GIT_TOOLDIST_WORK
     [ ! -d $gitworkdir/.git ] && createGitWorking
     cd $gitworkdir
 
-echo BEFORE:  `cat .git/info/exclude`
+#echo BEFORE:  `cat .git/info/exclude`
 
     echo working on $module in `pwd`
 
     #if this module branch already exists...
     if [ -r .git/refs/heads/$module ]; then
-	#switch to branch and update (should be a no-op!):
-	git co $module
+        #switch to branch and update (should be a no-op!):
+        git co $module
     else
-	#otherwise, create & switch to branch:
-	git co -b $module
+        #otherwise, create & switch to branch:
+        git co -b $module
     fi
 
-echo AFTER:  `cat .git/info/exclude`
+#echo AFTER:  `cat .git/info/exclude`
+
     #update from cvs tools dist:
     bldmsg -p $p -markbeg getting cvs updates from $CVS_TOOLDIST_PATH
-    cvs -Q -f -r -d $CVS_TOOLDIST_PATH co $module
+    cvs -q -f -r -d $CVS_TOOLDIST_PATH co $module
     bldmsg -p $p -markend -status $? getting cvs updates from $CVS_TOOLDIST_PATH
 
     bldmsg -p $p -markbeg adding files for $module to git
@@ -112,4 +246,4 @@ echo AFTER:  `cat .git/info/exclude`
     bldmsg -p $p -markend -status $? pushing files for $module to $GIT_TOOLDIST_PATH
 done
 
-exit 0
+exit $exit_status
